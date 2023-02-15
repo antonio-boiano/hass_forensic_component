f5a81dbc6644a804b7b40a2ab4ca5f8ef4d55852
antonio-boiano <antonio.boiano@polimi.it>
2023-02-16 00:44:59 +0100

    add some tests for papers

---
+++ test.py
@@ -0,0 +1,44 @@
+import asyncio
+from core import IotForensics
+
+async def main():
+    iot_forensics:IotForensics = IotForensics()
+
+    snf_file_path = "./forensic_capture"
+        
+
+    feat_cfg_dict_tmp = {
+    "topology_map": False,
+    "time_window": 5,
+    #"timestamp":,
+    #"relative_time":,
+    #"length":,
+    #"payload_data_length":,
+    #"dbm":,
+    #"formatting_type":,
+    #"src":,
+    #"dest":,
+    #"incoming":,
+    #"outgoing":,
+    #"mean_inter_arrival_time":,
+    #"mean_size": ,
+    #"mean_payload_size":,
+    #"std_inter_arrival_time":,
+    #"std_size":,
+    #"std_payload_size":,
+    #"mad_inter_arrival_time":,
+    #"mad_size":,
+    #"mad_payload_size":,
+    #"csv_separator":,
+    "pcap_file_path": "/home/antonio/Desktop/PCPA_REPO/generic_test.pcapng"
+    }
+
+    feat_cfg_dict ={k: v for k, v in feat_cfg_dict_tmp.items() if v is not None}
+                 
+    await iot_forensics.start_features_capture(snf_file_path=snf_file_path,object_id=None,features_config=feat_cfg_dict, blocking=True)
+
+if __name__ == "__main__" :
+    loop = asyncio.get_event_loop()
+    #Insert here the function to test
+    loop.create_task(main())
+    loop.run_forever()
